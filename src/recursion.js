3666566b7c3cee93fcecc96b8191fd2f00eaaecf
conniechu929 <conniechu929@gmail.com>
2016-06-02 14:52:34 -0700

    Completed writing dividing a number without using operator using recursion

--- src/recursion.js
+++ src/recursion.js
@@ -170,6 +170,21 @@ var multiply = function(x, y) {
 // 13. Write a function that divides two numbers without using the / operator  or
 // JavaScript's Math object.
 var divide = function(x, y) {
+	if(x < y) {
+		return 0;
+	}
+	else if(x === 0 || y === 0) {
+		return NaN;
+	}
+	else if(x < 0) {
+		return -divide(-x, y);
+	}
+	else if(y < 0) {
+		return -divide(x, -y);
+	}
+	else {
+		return 1 + divide(x - y, y)
+	}
 };
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers.  The GCD of two
